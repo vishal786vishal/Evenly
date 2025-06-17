062e4821f116a9042f32077238f82444e6c10cad
vishal786vishal <vishal.chugh1010@gmail.com>
2025-06-17 17:22:31 +0530

    Update AppError.java

--- eclipse-workspace/Evenly/src/main/java/com/qa/evenly/errors/AppError.java
+++ eclipse-workspace/Evenly/src/main/java/com/qa/evenly/errors/AppError.java
@@ -1,5 +1,8 @@
-package com.qa.evenly.errors;
+package com.qa.opencart.errors;
 
 public class AppError {
+	
+	public static final String INVALID_BROWSER_MESG = "=====please pass the right browser name===";
+
 
 }
